edb17b53b37d11c17a2d4cbbba107bcb43c4896c
Oleksii Kovalov <okovalov@e1r3p11.unit.ua>
2017-11-12 19:48:38 +0200

    gr

--- main.cpp
+++ main.cpp
@@ -1,14 +1,53 @@
 
 #include "CPU.hpp"
-#include "Network.hpp"
+#include <SFML/Graphics.hpp>
 
-int main(void) {
-    CPU h;
-    h.upData();
-    std::cout << h.getCPU() << std::endl;
-    std::cout << h.getCPULoad() << std::endl;
-    Network n;
-    n.upData();
-    std::cout << n.getButes() << std::endl;
-    std::cout << n.getPuckets() << std::endl;
-}
+int main(void)
+    {
+        sf::RenderWindow window(sf::VideoMode(1200, 600), "ft_gkrellm");
+
+        sf::Text text;
+    sf::Font font;
+        font.loadFromFile("GloriaHallelujah.ttf");
+// select the font
+    text.setFont(font); // font is a sf::Font
+
+// set the string to display
+    text.setString("Hello world");
+
+// set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+// set the color
+    text.setFillColor(sf::Color::Black);
+    text.setPosition(0, 0);
+
+// set the text style
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+
+
+
+    while (window.isOpen())
+        {
+            // check all the window's events that were triggered since the last iteration of the loop
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                // "close requested" event: we close the window
+
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            // clear the window with black color
+            window.clear(sf::Color::White);
+
+            // draw everything here...
+            // window.draw(...);
+            window.draw(text);
+            // end the current frame
+            window.display();
+        }
+        return 0;
+    }q
