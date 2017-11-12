f25750f44698709edca36bbee74079f436ca476f
Nick <nekit_p@ukr.net>
2017-11-12 14:32:06 +0200

    was not-up-to date. merge2

--- IMonitorModule.hpp
+++ IMonitorModule.hpp
@@ -1,18 +1,31 @@
 
 #ifndef IMONITORMODULE_HPP
-#define IMONITORMODULE_HPP
+# define IMONITORMODULE_HPP
 
+# include <iostream>
+# include <ctime>
+# include <sys/types.h>
+# include <sys/sysctl.h>
+# include <iostream>
+# include <sstream>
+# include <sys/sysctl.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <mach/mach_host.h>
 
 class IMonitorModule {
 
 protected:
     int _index;
+    bool _exist;
 
 public:
     IMonitorModule();
     IMonitorModule(const IMonitorModule &obj);
     virtual void upData(void) = 0;
     int getIndex(void) const;
+    bool getExist(void) const;
+    void setExist(void);
     IMonitorModule &operator=(const IMonitorModule &obj);
     ~IMonitorModule();
 };
